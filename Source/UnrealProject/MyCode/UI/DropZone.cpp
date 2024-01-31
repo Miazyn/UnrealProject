a36b3666c08dbad8294ddf7852bfba0771d2de0a
Miazyn <94353989+Miazyn@users.noreply.github.com>
2024-01-31 23:42:50 +0100

    Strawberry dropped

--- Source/UnrealProject/MyCode/UI/DropZone.cpp
+++ Source/UnrealProject/MyCode/UI/DropZone.cpp
@@ -3,9 +3,35 @@
 
 #include "DropZone.h"
 
+#include "DragDrop.h"
+#include "InventoryItemWidget.h"
+#include "InventoryWidget.h"
+#include "Blueprint/WidgetBlueprintLibrary.h"
+
 bool UDropZone::NativeOnDrop(const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent,
-	UDragDropOperation* InOperation)
+                             UDragDropOperation* InOperation)
 {
+	Super::NativeOnDrop(InGeometry, InDragDropEvent, InOperation);
+	
 	UE_LOG(LogTemp, Log, TEXT("Dropped on DropZone"));
-	return Super::NativeOnDrop(InGeometry, InDragDropEvent, InOperation);
+	
+	if (InOperation)
+	{
+		UE_LOG(LogTemp, Log, TEXT("Dropped 1on DropZone"));
+		
+			
+			UDragDrop* DragDropOp = Cast<UDragDrop>(InOperation);
+
+			if(DragDropOp)
+			{
+				UE_LOG(LogTemp, Log, TEXT("Dropped3 on DropZone"));
+				UInventoryItemWidget* Widget = Cast<UInventoryItemWidget>(DragDropOp->WidgetReference);
+				if(Widget){
+					UE_LOG(LogTemp,Log, TEXT("Dropped %s"), *Widget->InventorySlotItem->ItemName);
+				}
+			}
+		
+	}
+	
+	return true;
 }
